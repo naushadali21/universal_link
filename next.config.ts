f72568216046b1bbd64f9d89ae1c7d7911e297f8
naushadali789 <naushadali42@gmail.com>
2025-07-10 16:02:12 +0530

    support multiple  WLPs

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,42 @@ module.exports = {
         source: '/uLink/:path*',
         destination: '/api/uLink/:path*',
       },
+      {
+        source: '/atlantida/:path*',
+        destination: '/api/atlantida/:path*',
+      },
+      {
+        source: '/remesasbi/:path*',
+        destination: '/api/remesasbi/:path*',
+      },
+      {
+        source: '/everpayer/:path*',
+        destination: '/api/everpayer/:path*',
+      },
+      {
+        source: '/remesasbt/:path*',
+        destination: '/api/remesasbt/:path*',
+      },
+      {
+        source: '/mlremit/:path*',
+        destination: '/api/mlremit/:path*',
+      },
+      {
+        source: '/ussc/:path*',
+        destination: '/api/ussc/:path*',
+      },
+      {
+        source: '/miotransfer/:path*',
+        destination: '/api/miotransfer/:path*',
+      },
+      {
+        source: '/remesasreservas/:path*',
+        destination: '/api/remesasreservas/:path*',
+      },
+      {
+        source: '/mazremit/:path*',
+        destination: '/api/mazremit/:path*',
+      },
     ]
   },
 };
